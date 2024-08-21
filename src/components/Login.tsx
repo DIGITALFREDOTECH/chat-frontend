68c167a2b8766f795aed679efca94dfd2fa45809
DIGITALFREDOTECH <162149183+DIGITALFREDOTECH@users.noreply.github.com>
2024-08-21 04:39:33 +0100

    initial commit

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -36,7 +36,7 @@ function Login() {
       <h1 className="text-3xl font-bold mb-4">Chat Login</h1>
       <form
         onSubmit={handleSubmit}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        className="bg-blue shadow-md rounded px-8 pt-6 pb-8 mb-4"
       >
         <div className="mb-4">
           <label
